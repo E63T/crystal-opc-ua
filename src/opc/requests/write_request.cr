01815d94a0809c702201f234ec67785997e880fb
unn4m3d <smelnikov871@gmail.com>
2022-01-14 12:55:05 +1000

    Another fix

--- src/opc/requests/write_request.cr
+++ src/opc/requests/write_request.cr
@@ -10,6 +10,14 @@ module OPC
       wr
     end
 
+    def self.new(node : NodeID, attribute, value)
+      wr = WriteValue.new
+      wr.node_id = node
+      wr.attribute_id = attribute
+      wr.value = value
+      wr
+    end
+
     custom node_id : NodeID = NodeID.new
     uint32 attribute_id
 
